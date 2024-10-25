fd2ce2ca9d3c7054864cfb00ef8dac758c2c7116
Kertul <impasbezkonfliktowy@gmail.com>
2024-10-25 11:24:57 +0200

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+
+    let k = 6;
+
+    println!( "{}", k.to_string() );
+    println!( "{}", test(k).to_string() );
+    println!( "{}", k.to_string() );
+}
+
+
+fn test( mut var: u64 ) -> u64 {
+    let var2: u64 = 11;
+    var = 21;
+    println!( "{}", var.to_string() );
+    var2
+}
+
+#[test]
+fn test_test() {
+    assert_eq!( test(5), 11 );
 }
